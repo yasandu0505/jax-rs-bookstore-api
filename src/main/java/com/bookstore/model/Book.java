b493888af4c9b2bbc359bd56fcfc06492aa103e4
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-04-26 18:28:35 +0530

    feat: add book model getters and setters

--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -8,6 +8,9 @@ package com.bookstore.model;
  *
  * @author yasandu
  */
+import javax.xml.bind.annotation.XmlRootElement;
+
+
 public class Book {
     private int id;
     private String title;
@@ -16,4 +19,89 @@ public class Book {
     private int publicationYear;
     private double price;
     private int stockQuantity;
+    
+    
+    // All-args constructor
+    public Book(int id, String title, String author, String isbn, int publicationYear, double price, int stockQuantity) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    // Getters and Setters (Important for JAX-RS JSON mapping)
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    // Optionally override toString() for debugging
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", price=" + price +
+                ", stockQuantity=" + stockQuantity +
+                '}';
+    }
+    
 }
